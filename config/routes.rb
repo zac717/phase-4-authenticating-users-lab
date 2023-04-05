51d250f0d36626be562537614a06f9e564d9d96b
Isaac Ongera <isaacongera717@gmail.com>
2023-04-05 08:31:19 +0300

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Login
+  post "/login", to: "sessions#create"
+  # Stay logged-in
+  get "/me", to: "users#show"
+  # Logout
+  delete "/logout", to: "sessions#destroy"
 end
